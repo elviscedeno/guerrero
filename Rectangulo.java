f1b1ab307e409e61036ba4b9e6aa903f07b11b2f
elviscedeno <elvis.cedenog@ug.edu.ec>
2017-05-06 19:51:14 -0500

    Add files via upload

---
+++ Rectangulo.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller_grupal;
+
+/**
+ *
+ * @author elvis
+ */
+public class Rectangulo {
+ 
+    double ancho;
+    double altura;
+
+    public Rectangulo() {
+        this.ancho = 1.00d;
+        this.altura = 1.00d;
+    }
+
+    public Rectangulo(double ancho, double altura) {
+        this.ancho = ancho;
+        this.altura = altura;
+    }
+    
+    public double getArea(){
+        return ancho * altura;
+    }
+    
+    public double getPerimetro(){
+        return (ancho + altura) * 2;
+    }
+    
+    public static void main(String [] args){
+        Rectangulo recta = new Rectangulo();
+        recta.getArea();
+        recta.getPerimetro();
+    }
+}
