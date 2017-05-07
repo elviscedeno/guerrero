d7fee52475d0c31979e0d43523214b0cb4919002
elviscedeno <elvis.cedenog@ug.edu.ec>
2017-05-06 22:00:05 -0500

    Add files via upload

---
+++ Empleado.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio_4;
+
+import java.util.Date;
+
+/**
+ *
+ * @author elvis
+ */
+public class Empleado  extends Persona{
+    double salario;
+    Date fecha_entrada;
+}
---
+++ Persona.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio_4;
+
+/**
+ *
+ * @author elvis
+ */
+public class Persona {
+    String nombre;
+    String direccion;
+    String telefono;
+    String email;
+}
